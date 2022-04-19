a52d0e50138090ec96ef56974528307f0ba7ce0a
sonam <sonamsahu199518@gmail.com>
2022-04-19 23:10:24 +0530

    Add project files.

---
+++ Polymophism1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymophism1
+{
+
+    //polymphism is one of the primary pillars of the object orianted programing language.
+    //polymorphism allows us to invoked drived class methode through base class refrence variable.
+    
+    public  class Customer
+    {
+        private string CreateCustomer()
+        {
+            return null;
+        }
+        protected string CreateCustomer1()
+        {
+            return null;
+        }
+    }
+    public class Order: Customer
+    {
+        public string CreateCustomer()
+        {
+            Customer c = new Customer();
+            Order o = new Order();
+            
+            return null;
+        }
+    }
+}
